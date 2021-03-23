e878170f228c16482ce29aad6ef10666a60b062d
romuald <romuald.lecorroller@gmail.com>
2021-03-23 09:01:47 +0100

    remodif

--- 02_programming/01_algorithms_day_1/00718e83d9bddcfe9241cdbcb5923c0773feb21d/05_play_with_while_loop/src/04_filtering_with_while.js
+++ 02_programming/01_algorithms_day_1/00718e83d9bddcfe9241cdbcb5923c0773feb21d/05_play_with_while_loop/src/04_filtering_with_while.js
@@ -3,11 +3,11 @@
 // -  Create an array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each odd values of the table.
 const litteralDigits = ["zero","one","two","three","four","five","six","seven","eight","nine"];
+const test = [0,1,2,3,4,5,6,7,8,9];
 let i = 0;
-console.log(litteralDigits);
-while (i < litteralDigits.length) {
-  litteralDigits[i].split("");
-  
+let newTab =[];
+
+while (i < test.length) {
+  newTab = test.filter(valeur => valeur%2===1 ? true : false);
   i++;
 }
-console.log(litteralDigits);
