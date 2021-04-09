76778db080a7d893040dbf0a311a53f316bbac61
romuald <romuald.lecorroller@gmail.com>
2021-04-09 17:18:03 +0200

    exo github sur promises fini

--- 03_backend_web_development_and_api/01_promises/a15b4e3fc4cb48d99dab2e50e0efa06f38382289/03_github_client/src/github-client.ts
+++ 03_backend_web_development_and_api/01_promises/a15b4e3fc4cb48d99dab2e50e0efa06f38382289/03_github_client/src/github-client.ts
@@ -1,4 +1,5 @@
 import { Response } from "node-fetch";
+import { report } from "node:process";
 import { getReposUrlByNickname, listRepos, getOneRepoInfos } from "../utils";
 
 export type Repo = {
@@ -30,13 +31,21 @@ export class GithubClient {
 
   static printRepos(data: Repo[]): Promise<string | Repo[]> {
     // You code goes here
-    console.log(data);
+
+    data.forEach((repo, index) => {
+      console.log(`${index + 1} . ${repo.name}`);
+    });
     return data;
   }
 
   static printRepository(newrepo: Repo): Promise<string | Repo> {
     // You code goes here
-    console.log(`this is a ${newrepo.description}`);
+    console.log(newrepo.name);
+    console.log(newrepo.description);
+    console.log(newrepo.subscribers_count);
+    console.log(newrepo.stargazers_count);
+    console.log(newrepo.language);
+    console.log(newrepo.url);
   }
 
   static getProjectInformations(url: string): Promise<string | Repo> {
