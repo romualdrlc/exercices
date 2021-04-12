beec482a60cc6ec1cc36903d1992f51e807f6186
romuald <romuald.lecorroller@gmail.com>
2021-04-12 08:49:15 +0200

    derniere modif avant la repsise

--- 03_backend_web_development_and_api/01_promises/a15b4e3fc4cb48d99dab2e50e0efa06f38382289/04_github_user_interface/src/github-client.ts
+++ 03_backend_web_development_and_api/01_promises/a15b4e3fc4cb48d99dab2e50e0efa06f38382289/04_github_user_interface/src/github-client.ts
@@ -1,1 +1,59 @@
 // Paste the content of your GithubClient file here
+import { Response } from "node-fetch";
+import { report } from "node:process";
+
+import { getReposUrlByNickname, listRepos, getOneRepoInfos } from "../utils";
+
+export type Repo = {
+  url: string;
+  name: string;
+  [key: string]: string | number | boolean | null;
+};
+
+export type GitHub = {
+  message: string;
+  repos_url: string;
+};
+
+export class GithubClient {
+  static getReposUrl(nickname: string): Promise<string | undefined> {
+    // You code goes here
+    return getReposUrlByNickname(nickname);
+  }
+
+  static getRepos(url: string): Promise<Repo[]> {
+    // You code goes here
+    return listRepos(url);
+  }
+
+  static printRepos(data: Repo[]): Repo[] {
+    // You code goes here
+
+    data.forEach((repo, index) => {
+      console.log(`${index + 1} . ${repo.name}`);
+    });
+    return data;
+  }
+
+  static printRepository(newrepo: Repo): void {
+    // You code goes here
+    console.log(newrepo.name);
+    console.log(newrepo.description);
+    console.log(newrepo.subscribers_count);
+    console.log(newrepo.stargazers_count);
+    console.log(newrepo.language);
+    console.log(newrepo.url);
+  }
+
+  static getProjectInformations(url: string): Promise<string | Repo> {
+    // You code goes here
+    return getOneRepoInfos(url);
+  }
+}
+
+// .then((data) => {
+//   if (data.message === "Not Found") {
+//     throw new Error("This User doesn't exist");
+//   }
+//   return data.repos_url;
+// });
--- 03_backend_web_development_and_api/01_promises/a15b4e3fc4cb48d99dab2e50e0efa06f38382289/04_github_user_interface/src/index.ts
+++ 03_backend_web_development_and_api/01_promises/a15b4e3fc4cb48d99dab2e50e0efa06f38382289/04_github_user_interface/src/index.ts
@@ -1,6 +1,8 @@
 import { ask, closeRl } from "../interface";
+import { getOneRepoInfos, listRepos } from "../utils";
 import { GithubClient, Repo } from "./github-client";
 
 ask("Github nickname\n> ")
-  .then((nickname) => GithubClient.getReposUrl(nickname))
-  // Continue to chain here
+.then((nickname) => GithubClient.getReposUrl(nickname));
+.then((descrepo) => GithubClient.printRepos(descrepo))
+// Continue to chain here
