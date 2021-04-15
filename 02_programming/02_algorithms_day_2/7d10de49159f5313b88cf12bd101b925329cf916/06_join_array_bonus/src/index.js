2e9fc66b851da7874d1a656452141cb5feb7a9cf
romuald <romuald.lecorroller@gmail.com>
2021-04-15 19:03:57 +0200

    reduce algo day 2 bonusexo

--- 02_programming/02_algorithms_day_2/7d10de49159f5313b88cf12bd101b925329cf916/06_join_array_bonus/src/index.js
+++ 02_programming/02_algorithms_day_2/7d10de49159f5313b88cf12bd101b925329cf916/06_join_array_bonus/src/index.js
@@ -1,5 +1,13 @@
-function joinArray() {
+function joinArray(tab, carac) {
   // Code your function here.
+  const tabvide = [];
+  tab.reduce((newtab, index) => {
+    tab.forEach((element) => {
+      newtab.push(tab[element] + carac);
+    });
+    console.log(newtab);
+    return newtab;
+  }, []);
 }
 
 // ⚠ Do not remove me ! It's for tests
