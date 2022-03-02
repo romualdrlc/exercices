d044817b5abe4936c2a330d46cc6c17ad3538e54
romuald <romuald.lecorroller@gmail.com>
2022-03-02 15:35:53 +0100

    exo 04_number_game_with_stats termine

--- 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/04_number_game_with_stats/src/numberGameWithStats.js
+++ 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/04_number_game_with_stats/src/numberGameWithStats.js
@@ -3,24 +3,31 @@ function numberGameWithStats(reader, min = 1, max = 100) {
   const entierAleatoire = Math.round(Math.random() * (max - min) + min);
   console.log("welcome to my game");
   let counter = 11;
+  console.log(`vous avez ${counter - 1} tentative`);
   function loop(number) {
     counter = counter - 1;
     let numero = parseInt(number);
     if (counter > 0) {
       if (isNaN(numero)) {
+        reader.question("Enter a number", loop);
         console.log(`This was not a number il vous reste ${counter} tentative`);
-        reader.question("Enter a number", (loop));
       } else if (numero < min || numero > max) {
         console.log(`The number is between 1 and 100 il vous reste ${counter} tentative`);
-        reader.question("Enter a number", (loop));
+        reader.question("Enter a number", loop);
       } else if (numero < entierAleatoire) {
+        reader.question("Enter a number ", loop);
         console.log(`Too low il vous reste ${counter} tentative`);
-        reader.question("Enter a number ", (loop));
       } else if (numero > entierAleatoire) {
+        reader.question("Enter a number", loop);
         console.log(`Too high il vous reste ${counter} tentative`);
-        reader.question("Enter a number", (loop));
-      } if (numero === entierAleatoire) {
-        console.log(`You won! on ${counter} fois`);
+      }
+      if (numero === entierAleatoire && counter === 9) {
+        console.log("You won! on 1 time !!!!!!!!");
+        reader.close();
+      }
+      if (numero === entierAleatoire) {
+        console.log(`You won! on ${10 - counter} fois`);
+        console.log(10 - counter);
         reader.close();
       }
     } else {
@@ -28,7 +35,7 @@ function numberGameWithStats(reader, min = 1, max = 100) {
       reader.close();
     }
   }
-  reader.question("Enter a number", (loop(counter)));
+  reader.question("Enter a number", loop(counter));
 }
 
 module.exports = numberGameWithStats;
